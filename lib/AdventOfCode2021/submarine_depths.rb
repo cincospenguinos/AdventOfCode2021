9891df24734e5745fb200fe5a3ff58d3fffd2acf
Andre LaFleur <andre.lafleur@techcyte.com>
2021-12-01 10:28:51 -0700

    Finish part two of day one

--- lib/AdventOfCode2021/submarine_depths.rb
+++ lib/AdventOfCode2021/submarine_depths.rb
@@ -2,18 +2,41 @@ module AdventOfCode2021
 	class SubmarineDepths
 		def initialize(depth_string)
 			@depths = depth_string.split(/\s+/).map(&:to_i)
-			@increases = nil
+			@dual_increases = nil
+			@sum_increases = nil
+			@sum_array = nil
 		end
 
-		def increases
-			return @increases unless @increases.nil?
+		def dual_increases
+			return @dual_increases unless @dual_increases.nil?
 
-			@increases = 0
-			(1...(@depths.size)).each do |index|
-				@increases += 1 if @depths[index - 1] < @depths[index]
+			@dual_increases = increases_for(@depths)
+			@dual_increases
+		end
+
+		def sum_increases
+			return @sum_increases unless @sum_increases.nil?
+
+			@sum_increases = increases_for(sums)
+			@sum_increases
+		end
+
+		private
+
+		def sums
+			return @sum_array unless @sum_array.nil?
+			@sum_array = []
+			(2...(@depths.size)).each { |index| @sum_array << @depths[(index - 2)..index].sum }
+			@sum_array
+		end
+
+		def increases_for(array)
+			increases = 0
+			(1...(array.size)).each do |index|
+				increases += 1 if array[index - 1] < array[index]
 			end
 
-			@increases
+			increases
 		end
 	end
 end
--- test/submarine_depths_test.rb
+++ test/submarine_depths_test.rb
@@ -14,14 +14,25 @@ class SubmarineDepthsTest < Minitest::Test
 		263
 	DEPTHS
 
+	DAY_ONE_INPUT = File.open('test/test_data/day_one.txt', 'r', &:read)
+
 	def test_has_correct_number_of_increases
 		depths = AdventOfCode2021::SubmarineDepths.new(EXAMPLE_DEPTHS)
-		assert_equal 7, depths.increases
+		assert_equal 7, depths.dual_increases
 	end
 
 	def test_answers_part_one
-		day_one = File.open('test/test_data/day_one.txt', 'r', &:read)
-		depths = AdventOfCode2021::SubmarineDepths.new(day_one)
-		assert_equal 1228, depths.increases
+		depths = AdventOfCode2021::SubmarineDepths.new(DAY_ONE_INPUT)
+		assert_equal 1228, depths.dual_increases
+	end
+
+	def test_has_correct_number_of_sum_increases
+		depths = AdventOfCode2021::SubmarineDepths.new(EXAMPLE_DEPTHS)
+		assert_equal 5, depths.sum_increases
+	end
+
+	def test_answers_part_two
+		depths = AdventOfCode2021::SubmarineDepths.new(DAY_ONE_INPUT)
+		assert_equal 1257, depths.sum_increases
 	end
 end
