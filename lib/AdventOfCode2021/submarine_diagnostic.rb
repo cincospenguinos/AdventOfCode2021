b5a25dffcfb2dac9587757ed80c1f8ad9a40e33b
Andre LaFleur <cincospenguinos@gmail.com>
2021-12-02 22:27:47 -0700

    Refactor in preparation for part two

--- lib/AdventOfCode2021/submarine_diagnostic.rb
+++ lib/AdventOfCode2021/submarine_diagnostic.rb
@@ -1,4 +1,22 @@
 module AdventOfCode2021
+  class BitFrequencyQuery
+    attr_reader :frequency_hash
+
+    def initialize(frequency_hash)
+      @frequency_hash = frequency_hash
+    end
+
+    def most_frequent
+      return '0' if frequency_hash['0'] > frequency_hash['1']
+      '1'
+    end
+
+    def least_frequent
+      return '1' if frequency_hash['0'] > frequency_hash['1']
+      '0'
+    end
+  end
+
   class SubmarineDiagnostic
     attr_reader :bytes_str
 
@@ -6,38 +24,41 @@ module AdventOfCode2021
       @bytes_str = report.split("\n")
       @gamma = nil
       @epsilon = nil
+      @oxygen_generator = nil
+      @co2_scrubber = nil
     end
 
     def gamma
-      calculate if @gamma.nil?
+      calculate_power if @gamma.nil?
       @gamma.to_i(2)
     end
 
     def epsilon
-      calculate if @epsilon.nil?
+      calculate_power if @epsilon.nil?
       @epsilon.to_i(2)
+    end
+
+    def power_consumption
+      gamma * epsilon
     end
 
     private
 
-    def calculate
+    def calculate_power
       @gamma = ''
       @epsilon = ''
-      length = bytes_str[0].length
 
-      (0...length).each do |index|
-        frequency_hash = bytes_str.map { |e| e[index] }
-          .group_by(&:itself)
-          .transform_values(&:size)
-
-        if frequency_hash['0'] > frequency_hash['1']
-          @gamma += '0'
-          @epsilon += '1'
-        else
-          @gamma += '1'
-          @epsilon += '0'
-        end
+      (0...bytes_str[0].length).each do |index|
+        bit_query = BitFrequencyQuery.new(frequency_hash_for(index))
+        @gamma += bit_query.most_frequent
+        @epsilon += bit_query.least_frequent
       end
+    end
+
+    def frequency_hash_for(index)
+      bytes_str.map { |e| e[index] }
+        .group_by(&:itself)
+        .transform_values(&:size)
     end
   end
 end
--- test/submarine_diagnostic_test.rb
+++ test/submarine_diagnostic_test.rb
@@ -16,6 +16,8 @@ class SubmarineDiagnosticTest < Minitest::Test
     01010
   POWER
 
+  DAY_THREE_INPUT = File.open('test/test_data/day_three.txt', 'r', &:read)
+
   def test_diagnostic_calculates_gamma
     diagnostic = AdventOfCode2021::SubmarineDiagnostic.new(EXAMPLE_INPUT)
     assert_equal 22, diagnostic.gamma
@@ -24,5 +26,15 @@ class SubmarineDiagnosticTest < Minitest::Test
   def test_diagnostic_calculates_epsilon
     diagnostic = AdventOfCode2021::SubmarineDiagnostic.new(EXAMPLE_INPUT)
     assert_equal 9, diagnostic.epsilon
+  end
+
+  def test_diagnostic_calculates_power_consumption
+    diagnostic = AdventOfCode2021::SubmarineDiagnostic.new(EXAMPLE_INPUT)
+    assert_equal 198, diagnostic.power_consumption
+  end
+
+  def test_diagnostic_calculates_first_part
+    diagnostic = AdventOfCode2021::SubmarineDiagnostic.new(DAY_THREE_INPUT)
+    assert_equal 3847100, diagnostic.power_consumption
   end
 end
